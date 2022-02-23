7bf8ad0cd3e2f4db22957de3a44af4715f5c8be5
zhangteng <zhangteng@q1.com>
2022-02-23 16:04:37 +0800

    update：Promise基本结构完成

--- 1_Promise结构设计.js
+++ 1_Promise结构设计.js
@@ -1,13 +1,32 @@
 // Promise/A+ 规范
 
+// 状态
+const PROMISE_STATUS_PENDING = 'pending'
+const PROMISE_STATUS_FULFILLED = 'fulfilled'
+const PROMISE_STATUS_REJECTED = 'rejected'
+
 class ZTPromise {
   constructor(executor) {
-    const resolve = () => {
-      console.log('resolve被调用')
+    // 默认pending状态
+    this.status = PROMISE_STATUS_PENDING
+    // 保存参数的变量，我们.then时需要用到
+    this.value = undefined
+    this.reason = undefined
+
+    const resolve = (value) => {
+      // 状态判断
+      if (this.status === PROMISE_STATUS_PENDING) {
+        this.status = PROMISE_STATUS_FULFILLED
+        this.value = value
+      }
     }
 
-    const reject = () => {
-      console.log('reject被调用')
+    const reject = (reason) => {
+      // 状态判断
+      if (this.status === PROMISE_STATUS_PENDING) {
+        this.status = PROMISE_STATUS_REJECTED
+        this.reason = reason
+      }
     }
 
     executor(resolve, reject)
@@ -15,7 +34,12 @@ class ZTPromise {
 }
 
 const promise = new ZTPromise((resolve, reject) => {
-  resolve()
-  reject()
+  resolve(111)
+  reject(222)
 })
 
+promise.then(res => {
+
+}, err => {
+
+})
